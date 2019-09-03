097aa4486f51a91b2435e3eac89d775c10d57de5
Pichon Théo <theo.pichon@etu.unilim.fr>
2019-09-03 16:14:52 +0200

    class personne

--- CarnetAdresse/src/Personne.java
+++ CarnetAdresse/src/Personne.java
@@ -1,4 +1,16 @@
 
 public class Personne {
 
+	private String nom ;
+	private String prenom;
+	private String dateNaissance;
+	private String numero;
+	private String adresse;
+	
+	public Personne(String nom, String prenom, String numero) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.numero = numero;
+		
+	}
 }
